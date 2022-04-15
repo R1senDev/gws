eb6b18e43f3b64595b9caa32f8446dfa0bd1dd32
Pavel Ovchinnikov <99139074+R1senDev@users.noreply.github.com>
2022-04-15 20:20:41 +0300

    Fixed filters

--- js/hub.js
+++ js/hub.js
@@ -10,29 +10,30 @@ let games = [new Game('uwu', ['antistress']), new Game('lineRace', ['race'])];
 let allFilters = ['antistress', 'race', 'platformer', 'clicker'];
 
 function switchFilter(value) {
-	alert(`switchFilter(${value})`);
+	for (let i of games) {
+		i.elem.style.opacity = 1;
+	}
 	for (let i of allFilters) {
 		document.getElementById(`${i}-filter`).style.color = 'black';
 	}
 	document.getElementById(`${value}-filter`).style.color = 'green';
-	if (value == null) {
+	if (value == 'null') {
 		for (let i of allFilters) {
 			document.getElementById(`${i}-filter`).style.color = 'black';
 		}
 		for (let i of games) {
+			alert(`Setting ${i.name}.style.opacity to 1`);
 			i.elem.style.opacity = 1;
 		}
 	} else {
 		document.getElementById(`${value}-filter`).style.color = 'green';
 		for (let i of games) {
 			if (i.tags.includes(value)) {
-				alert('Set opacity for', i.name, ' to 1');
 				i.elem.style.opacity = 1;
 			} else {
-				alert('Set opacity for', i.name, ' to 0.2');
 				i.elem.style.opacity = 0.2;
 			}
 		}
 	}
 }
-switchFilter(null);
+switchFilter('null');
